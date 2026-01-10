a4bbc860e61756208280204647cc1ac8c965a479
karim <karimnahiz18@gmail.com>
2026-01-10 13:53:10 +0100

    .

---
+++ p00/ex1/ft_plot_area.py
@@ -0,0 +1,5 @@
+#
+lenght = int(input("Entre lenght : "))
+width = int(input("Enter width : "))
+total = (lenght * width)
+print("the plot area is : " , total)
---
+++ p00/ex2/ft_harvest_total.py
@@ -0,0 +1,5 @@
+frs = int(input("day 1 harvest : "))
+sec = int(input("day 2 harvest : "))
+thr = int(input("day 3 harvest : "))
+total = (frs + sec + thr)
+print("total harvest is :",total)
