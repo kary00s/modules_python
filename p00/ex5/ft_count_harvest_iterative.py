3ad465667abbc3750479bab10a05d6ae18c270a4
Karim Nahiz <karimnahiz18@gmail.com>
2026-01-19 23:19:35 +0100

    .

---
+++ p00/ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,9 @@
+def harvest_iter():
+    days = int(input("Days until harvest : "))
+    i = 0
+    while i < days:
+        i += 1
+        print(f"day {i}")
+    print("harvest time .")
+
+harvest_iter()
---
+++ p00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,11 @@
+def harvest():
+
+    days = int(input("Days until harvest : "))
+    def harvest_recur(i=0):
+        if i == days :
+            print("harvest time .")
+        else :
+            print(f"day {i + 1}")
+            harvest_recur(i+1)
+    harvest_recur()
+harvest()
