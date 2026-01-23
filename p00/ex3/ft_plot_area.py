ab72836fd046e6fefe2fa322e6b4c5a99f0394b5
karim <karimnahiz18@gmail.com>
2026-01-23 04:03:55 +0100

    .

---
+++ p00/ex3/ft_plot_area.py
@@ -0,0 +1,5 @@
+age = int(input("Enter plant age in days :  "))
+if (age >= 60):
+    print("plant is ready to harvest")
+if (age < 60):
+    print("plant needs more time to grow")
---
+++ p00/ex4/ft_water_reminder.py
@@ -0,0 +1,5 @@
+Days = int(input("Days since last watering  : "))
+if (Days > 2):
+    print("water the plant")
+if (Days <= 2):
+    print("the plant is fine")
