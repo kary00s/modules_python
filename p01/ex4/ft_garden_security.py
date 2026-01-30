438ca38791a3df936e942058f443608e45616013
karim <karimnahiz18@gmail.com>
2026-01-30 18:05:35 +0100

    .

--- p01/ex4/ft_garden_security.py
+++ p01/ex4/ft_garden_security.py
@@ -1,44 +1,43 @@
 class SecurePlant:
-    def __init__(self, name, height, age):
+    def __init__(self, name, age, height):
         self.name = name
         self.__height = height
         self.__age = age
 
     def display(self):
         print(f"plant created : {self.name}")
+        self.set_age()
+        self.set_height()
+        print(f"Current plant: Rose ({self.__age}cm, 30 days)")
 
-    def set_age(self, age):
-        if age > 0:
-            self.__age = age
-            print(f"age updated: age {self.__height}cm [OK]")
+    def set_age(self):
+        if self.get_age() >= 0:
+            self.__age = self.get_age()
+            print(f"age updated: age {self.__age}cm [OK]\n")
         else:
-            print(f"Invalid operation attempted: age {self.__age}cm [REJECTED]")
+            print("Invalid operation attempted:"
+                  f"age {self.__age}cm [REJECTED]")
+            print("Security: Negative age rejected\n")
 
-    def set_height(self, height):
-        if height > 0:
-            self.__height = height
-            print(f"Height updated: height {self.__height}cm [OK]")
+    def set_height(self):
+        if self.get_height() >= 0:
+            self.__height = self.get_height()
+            print(f"Height updated: height {self.__height}cm [OK]\n")
         else:
             print("Invalid operation attempted:"
                   f" height {self.__height}cm [REJECTED]")
-
+            print("Security: Negative height rejected\n")
+            
     def get_age(self):
         return self.__age
 
     def get_height(self):
         return self.__height
 
-
-plants = {}
-plants["Rose"] = SecurePlant("Rose", 25, -30)
-plants["Oak"] = SecurePlant("Oak", -200, 365)
-plants["Cactus"] = SecurePlant("Cactus", -5, 90)
-plants["jasmine"] = SecurePlant("jasmine", -50, 65)
-
+plant{}
+[]
 if __name__ == "__main__":
+    p = SecurePlant("jasmine", -10, 1)
     print("=== Garden Security System ===\n")
-    for p in plants.values():
-        p.display()
-        p.set_age(p.get_age())
-        p.set_height(p.get_height())
-        print("\n")
+    p.display()
+    print("\n")
--- p01/ex5/test.py
+++ p01/ex5/test.py
@@ -2,12 +2,39 @@ class Plant:
     def __init__(self, name, age, height):
         self.name = name
         self.age = age
-        self.height= height
+        self.height = height
 
-    def display(self)
-        print(f"name : ({self.name}) height ({self.height}) age :{self.age} ")
 
 class tree(Plant):
-    def __init__(self ,name , age, height, diameter):
+    def __init__(self, name, age, height, diameter, shade):
         Plant.__init__(self, name, age, height)
         self.diameter = diameter
+        self.shade = shade
+    
+    def produce_shade(self):
+        print(f"{self.name} (Tree) : {self.height} cm, {self.age} days, {self.diameter} cm diameter")
+        print(f"Oak provides {self.shade} square meters of shade")
+
+
+class flower(Plant):
+    def __init__(self, name, age, height, color):
+        Plant.__init__(self, name, age, height)
+        self.color = color
+    
+    def bloom(self):
+        print(f"{self.name} (Flower) : {self.height} cm, {self.age} days, {self.color} cm diameter")
+        print("Rose is blooming beautifully!")
+
+
+class Vegetable(Plant):
+    def __init__(self, name, age, harvest_season,  nutritional_value):
+        Plant.__init__(self, name, age, None)
+        self.harvest_season = harvest_season
+        self.nutritional_value = nutritional_value
+    
+    def harvest(self):
+        print(f"{self.name} (Vergetebale) : {self.height} cm, {self.age} days,"
+              f"{self.harvest_season} cm diameter")
+        print(f"Tomato is rich in vitamin {self.nutritional_value}")
+def display():
+    
