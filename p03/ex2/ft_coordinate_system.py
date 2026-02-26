f24ce8484ecb6112078a6de252c082164c98d337
Karim Nahiz <karimnahiz18@gmail.com>
2026-02-26 03:43:30 +0000

    kanahiz

--- p03/ex2/ft_coordinate_system.py
+++ p03/ex2/ft_coordinate_system.py
@@ -1,12 +1,48 @@
 import math
 
-def ft_coordinate_system():
-    args = sys.argv
-    i = 1
-    lst = []
-    while  i < len(args):
-        lst.append(args[i])
-        i += 1
-    print(lst)
+def get_cordinates(cordinate: list):
+    x = cordinate[0]
+    y = cordinate[1]
+    z = cordinate[2]
+    return x, y, z
 
-ft_coordinate_system()
+def creator(cordinate: list):
+    x, y, z = get_cordinates(cordinate)
+    print(f"Position Created : ({x}, {y}, {z})")
+
+def calculator(cor1, cor2=(0, 0, 0)):
+    x1, y1, z1 = get_cordinates(cor1)
+    x2, y2, z2 = get_cordinates(cor2)
+
+    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2)
+    print(f"Distance between ({x2}, {y2}, {z2}) and "
+        f"({x1}, {y1}, {z1}): {distance}")
+
+def check_cordinates(cordinate):
+    for position in cordinate:
+        if int(position):
+            pass
+        else:
+            raise ValueError("Error parsing coordinates: invalid"
+                            f"literal for int() with base 10: {position}")
+
+
+def Parsing(position: str):
+    cordinate = position.split(",")
+    try:
+        check_cordinates(cordinate)
+    except ValueError as error:
+        print(error)
+    return cordinate
+
+
+def ft_coordinate_system(cordinate: str):
+    cordinate_list = Parsing(cordinate)
+    
+
+
+def test_cordinates():
+    ## test without parsing   (10, 20, 5)
+
+    ft_coordinate_system("4,46,6")
+test_cordinates()
