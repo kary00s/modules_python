ef343bc47244969ba6f6d1cab540da88c439c87e
Karim Nahiz <karimnahiz18@gmail.com>
2026-04-09 06:28:01 +0100

    kanahiz

--- p09/ex0/space_station.py
+++ p09/ex0/space_station.py
@@ -1,13 +1,67 @@
-from pydantic import field
-class SpaceStation():
-    station_id = field( 3-10 )
-    name =String, 1-50 characters
-    crew_size =  Integer, 1-20 people
-    power_level =  Float, 0.0-100.0 percent
-    oxygen_level =  Float, 0.0-100.0 percent
-    last_maintenance =  DateTime field
-    is_operational =  Boolean, defaults to True
-    notes =  Optional string, max 200 characters
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+class SpaceStation(BaseModel):
+    station_id: str = Field(..., min_length=3, max_length=10)
+    name: str = Field(..., min_length=1, max_length=50)
+    crew_size: int = Field(..., ge=1, le=20)
+    power_level: float = Field(..., ge=0.0, le=100.0)
+    oxygen_level: float = Field(..., ge=0.0, le=100.0)
+    last_maintenance: datetime
+    is_operational: bool = True
+    notes: Optional[str] = Field(None, max_length=200)
+
 def main(): 
-    print("Space Station Data Validation")
-    print("========================================")
+    try:
+        valid = SpaceStation(
+            station_id="SS-001",
+            name="International Space Station",
+            crew_size=6,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime(2026, 1, 1, 9, 0, 0),
+            is_operational=True,
+            notes="All systems nominal.",
+        )
+
+        print("Space Station Data Validation")
+        print("========================================")
+        print("Valid station created:")
+        print(f"Station ID: {valid.station_id}")
+        print(f"Name: {valid.name}") 
+        print(f"Crew Size: {valid.crew_size} people") 
+        print(f"Power Level: {valid.power_level}%") 
+        print(f"Oxygen Level: {valid.oxygen_level}%") 
+        print(f"Last Maintenance: {valid.last_maintenance}") 
+        if valid.is_operational is True:
+            stats = "Operational" 
+        else:
+            stats = "Non-operational"
+        print(f"Status: {stats}") 
+        print(f"Notes: {valid.notes}") 
+    except Exception as e:
+        print(e)
+
+
+    try:
+        print("\n========================================")
+        print("Expected validation error:")
+        invalid = SpaceStation(
+            station_id="SS-002",
+            name="national Space Station",
+            crew_size=9999,
+            power_level=85.5,
+            oxygen_level=92.3,
+            last_maintenance=datetime(2026, 1, 1, 9, 0, 0),
+            is_operational=True,
+            notes="All systems nominal.",
+        )
+        print(f"Crew Size: {invalid.crew_size} people") 
+    except Exception as e:
+        print(e.errors()[0]["msg"], end="\n\n")
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
---
+++ p09/ex1/alien_contact.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, model_validator
+from typing import Optional
+from enum import Enum, auto
+
+
+class ContactType(Enum):
+    RADIO = auto()
+    VISUAL = auto()
+    PHYSICAL = auto()
+    TELEPATHIC = auto()
+
+
+class AlienContact(BaseModel):
+    contact_id: str = Field(min_length=5, max_length=15)
+    timestamp: datetime
+    location: str = Field(min_length=3, max_length=100)
+    contact_type: ContactType
+    signal_strength: float = Field(gt=0.0, lt=10.0)
+    duration_minutes: int = Field(ge=1, le=1440)
+    witness_count: int = Field(ge=1, le=100)
+    message_received: Optional[str] = Field(None, max_length=500)
+    is_verified: bool = False
+
+    @model_validator(mode='after')
+    def contact_id_validator(self):
+        if not self.contact_id.startswith("AC"):
+            raise ValueError("contact_id must start with 'AC'")
+        return self
+
+    @model_validator(mode='after')
+    def physical_validator(self):
+        if self.contact_type == ContactType.PHYSICAL and not self.is_verified:
+            raise ValueError("Physical contact reports must be verified")
+        return self
+
+    @model_validator(mode='after')
+    def telepathic_validator(self):
+        if self.contact_type == ContactType.TELEPATHIC and self.witness_count < 3:
+            raise ValueError("Telepathic contact requires at least 3 witnesses")
+        return self
+
+    @model_validator(mode='after')
+    def signal_validator(self):
+        if self.signal_strength > 7.0 and not self.message_received:
+            raise ValueError("Strong signals (> 7.0) should include a message")
+        return self
+
+
+def main():
+    try:
+        valid = AlienContact(
+            contact_id="AC_2026_001",
+            timestamp=datetime.now(),
+            location="Area 51, khouribga",
+            contact_type=ContactType.RADIO,
+            signal_strength=8.5,
+            duration_minutes=45,
+            witness_count=5,
+            message_received="Greetings from Zeta Reticuli",
+            is_verified=True,
+        )
+        
+        print("Alien Contact Log Validation")
+        print("======================================")
+        print("Valid contact report:")
+        print("ID: ", valid.contact_id)
+        print("Type: ", valid.contact_type)
+        print("Location: ", valid.location)
+        print("Signal: ", valid.signal_strength)
+        print("Duration: ", valid.duration_minutes)
+        print("Witnesses: ", valid.witness_count)
+        print("Message: ", valid.message_received)
+
+
+        print("\n======================================")
+        print("Expected validation error:")
+        invalid = AlienContact(
+            contact_id="AC_2026_002",
+            timestamp=datetime.now(),
+            location="Smoking Area, 1337",
+            contact_type=ContactType.TELEPATHIC,
+            signal_strength=8.5,
+            duration_minutes=45,
+            witness_count=2,
+            message_received="We will destroy your planet ",
+            is_verified=True,
+        )
+        print(invalid.witness_count)
+    except Exception as e:
+        print(e.errors()[0]["msg"])
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(e)
